a974ecbd63173c14ca59012c3985782d87ebaec0
matthew-modica <mmodica29@gmail.com>
2025-07-17 14:46:52 -0400

    Implement my_ls

--- shell_scheduler/include/tools.h
+++ shell_scheduler/include/tools.h
@@ -1,8 +1,5 @@
 #ifndef TOOLS_H
 #define TOOLS_H
 
-#include <dirent.h>
-
-void my_ls(DIR *dir);
 
 #endif
--- shell_scheduler/src/tools/my_ls.c
+++ shell_scheduler/src/tools/my_ls.c
@@ -1,6 +1,12 @@
-#include "tools.h"
+#include <dirent.h>
+#include <stdio.h>
 
 int main() {
-    
+    DIR *dir = opendir(".");
+
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL) {
+        printf("%s\n", entry->d_name);
+    }
     return 0;
 }

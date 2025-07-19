ca7dc7f11352c8a953119e1e7efdd79d5c4f4f72
matthew-modica <mmodica29@gmail.com>
2025-07-18 23:58:16 -0400

    Modify log 01

--- shell_scheduler/src/tools/my_ls.c
+++ shell_scheduler/src/tools/my_ls.c
@@ -1,12 +1,21 @@
 #include <dirent.h>
 #include <stdio.h>
 
-int main() {
-    DIR *dir = opendir(".");
+int main(int argc, char *argv[]) {
+    
+    char *input_dir;
+    if (argc == 1) {
+        input_dir = ".";
+    } else {
+        input_dir = argv[1];
+    }
+
+    DIR *dir = opendir(input_dir);
 
     struct dirent *entry;
     while ((entry = readdir(dir)) != NULL) {
         printf("%s\n", entry->d_name);
     }
+
     return 0;
 }

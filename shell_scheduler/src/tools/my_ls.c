efcaa846648ad68cd717d2a476355f2e66747554
matthew-modica <mmodica29@gmail.com>
2025-08-05 18:33:14 -0400

    Begin imlementation of option handling

--- shell_scheduler/src/tools/my_ls.c
+++ shell_scheduler/src/tools/my_ls.c
@@ -1,30 +1,26 @@
 #include <dirent.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <sys/stat.h>
+#include <getopt.h>
 
 int main(int argc, char *argv[]) {
-    struct stat st;
-    char *input_dir;
-    if (argc == 1) {
-        input_dir = ".";
-    } else {
-        input_dir = argv[1];
-    }
-
-    DIR *dir = opendir(input_dir);
-
+    struct stat *st;
     struct dirent *entry;
-    while ((entry = readdir(dir)) != NULL) {
-        printf("%s\n", entry->d_name);
+    char *path;
+
+    int opt;
+    while ((opt = getopt(argc, argv, "la") != -1)) {
+        switch (opt) {
+            case 'l':
+                break;
+            case 'a':
+                break;
+          default :
+                break;
+        }
     }
-       
-    /*
-     * Do this correctly
-     */
-    char *option1;
-    if (argc == 2) {
-        option1 = argv[2];
-    }
-    stat(entry->d_name, &st);
+
+    path = argv[optind];
     return 0;
 }

bf207516ebe510eacc65ed14b0678e624e9fd6b3
matthew-modica <mmodica29@gmail.com>
2025-08-05 22:01:21 -0400

    Implement -l flag handling

--- shell_scheduler/src/tools/my_ls.c
+++ shell_scheduler/src/tools/my_ls.c
@@ -1,18 +1,21 @@
 #include <dirent.h>
-#include <stdio.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <sys/stat.h>
 #include <getopt.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[]) {
     struct stat *st;
     struct dirent *entry;
     char *path;
+    int per_line = false;
 
     int opt;
     while ((opt = getopt(argc, argv, "la") != -1)) {
         switch (opt) {
             case 'l':
+                per_line = true;
                 break;
             case 'a':
                 break;
@@ -22,5 +25,12 @@ int main(int argc, char *argv[]) {
     }
 
     path = argv[optind];
+    DIR *dir = opendir(path);
+    while ((entry = readdir(dir)) != NULL) {
+        if (per_line) {
+            printf("%s\n", entry->d_name);
+        }
+    }
+
     return 0;
 }

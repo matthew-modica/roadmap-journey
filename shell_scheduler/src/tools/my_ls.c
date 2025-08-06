2d5830e45aee40b0354c65647db26a3a59316faf
matthew-modica <mmodica29@gmail.com>
2025-08-05 22:44:01 -0400

    Fix -a not setting boolean

--- shell_scheduler/src/tools/my_ls.c
+++ shell_scheduler/src/tools/my_ls.c
@@ -18,6 +18,7 @@ int main(int argc, char *argv[]) {
                 per_line = true;
                 break;
             case 'a':
+                include_dotfiles = true;
                 break;
           default :
                 break;

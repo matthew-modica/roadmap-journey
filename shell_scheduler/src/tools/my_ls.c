6588121ac0500972c93c287916f941e0d5f5d8f5
matthew-modica <mmodica29@gmail.com>
2025-08-08 01:55:57 -0400

    Create structure for ls options

--- shell_scheduler/src/tools/my_ls.c
+++ shell_scheduler/src/tools/my_ls.c
@@ -6,45 +6,49 @@
 #include <stdbool.h>
 
 int main(int argc, char *argv[]) {
-    struct dirent *entry;
+    struct LsOpts {
+        bool per_line;
+        bool include_dotfiles;
+        char *path;
+        struct dirent *entry;
+    }; 
+    
+    struct LsOpts ls_opts;
     struct stat entry_stats;
-    char *path;
-    bool per_line = false;
-    bool include_dotfiles = false;
 
     int opt;
     while ((opt = getopt(argc, argv, "la")) != -1) {
         switch (opt) {
             case 'l':
-                per_line = true;
+                ls_opts.per_line = true;
                 break;
             case 'a':
-                include_dotfiles = true;
+                ls_opts.include_dotfiles = true;
                 break;
             default :
                 break;
         }
     }
 
-    path = (optind < argc) ? argv[optind] : "."; // Use current directory if none specified
-    DIR *dir = opendir(path);
-    while ((entry = readdir(dir)) != NULL) {
-        if (!include_dotfiles && entry->d_name[0] == '.') {
+    ls_opts.path = (optind < argc) ? argv[optind] : "."; // Use current directory if none specified
+    DIR *dir = opendir(ls_opts.path);
+    while ((ls_opts.entry = readdir(dir)) != NULL) {
+        if (!ls_opts.include_dotfiles && ls_opts.entry->d_name[0] == '.') {
             continue;
         }
 
-        if (per_line) {
-            if (stat(entry->d_name, &entry_stats) == -1) {
+        if (ls_opts.per_line) {
+            if (stat(ls_opts.entry->d_name, &entry_stats) == -1) { // TODO: Can't use d_name here, must be path + d_name
                 perror("Error populating stats\n");
             } else {
                 printf("%li ", entry_stats.st_size);
             }
-            printf("%s\n", entry->d_name);
+            printf("%s\n", ls_opts.entry->d_name);
         } else {
-            printf("%s ", entry->d_name);    
+            printf("%s ", ls_opts.entry->d_name);    
         }
     }
-    if (!per_line) {
+    if (!ls_opts.per_line) {
         printf("\n");
     }
 
